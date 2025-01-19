a636b1a174778e18e986bdb83cc347905149bc8f
Alexis <abaleaalexis@gmail.com>
2025-01-19 15:07:17 +0100

    Correction valid url

--- src/app/shared/valid-url.pipe.ts
+++ src/app/shared/valid-url.pipe.ts
@@ -5,15 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ValidUrlPipe implements PipeTransform {
   transform(value: string): boolean {
-    const urlPattern = new RegExp(
-      '^(https?:\\/\\/)?(www\\.)?' + // protocol and www
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$',
-      'i',
-    ); // fragment locator
-    return !!urlPattern.test(value);
+    let url;
+
+    try {
+      url = new URL(value);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === 'http:' || url.protocol === 'https:';
   }
 }
