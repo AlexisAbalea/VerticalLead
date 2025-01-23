6900598b7d8c2eb4eeb015a275d03edea4528e31
Alexis <abaleaalexis@gmail.com>
2025-01-23 13:59:26 +0100

    ajout page de test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { DisplayCodeComponent } from './display-code/display-code.component';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './shared/auth/auth.guard';
+import { TestPageComponent } from './test-page/test-page.component';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
 
 export const routes: Routes = [
   { path: 'upload', component: UploadVideoComponent, canActivate: [authGuard] },
   { path: 'code-generated', component: DisplayCodeComponent, canActivate: [authGuard] },
+  { path: 'test-page', component: TestPageComponent, canActivate: [authGuard] },
   { path: '', component: LoginComponent },
   { path: '**', redirectTo: '' },
 ];
